f6e2c05e51204e164ff3307d6f8c91f58490442f
Neo <DavidEddlemon@Gmail.com>
2017-03-14 10:47:38 -0500

    There is no spoon...
    shamu: Stop logcat spam due to touchboost and mp-decision
    Thanks goes out to krexus-partners team and all others involved
    
    Speakerphone In-Call Echo Fix
    Thanks goes out to Yoinx and the rest of the DU team
    
    Boost that audio output! In-Call, Speakerphone, headphones, etc
    
    Revert "Disable VT on shamu"
    
    Remove the unnecessary decreased sound delays from notifications
    
    Smooth it out a bit
    
    A few more VoLTE changes
    
    Some Nougat bringup
    
    Change-Id: Ia9cd485bbf9f8612425cd8095fc55b18c01663cd
    Verizon: enable VVM3 visual voicemail

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,7 +159,8 @@ NXP_CHIP_TYPE := 2
 # Testing related defines
 BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/angler-setup.sh
 
-USE_CLANG_PLATFORM_BUILD := true
+# Use Snapdragon LLVM for Nightlies, if available
+TARGET_USE_SDCLANG := true
 
 # CMHW
 BOARD_USES_CYANOGEN_HARDWARE := true
